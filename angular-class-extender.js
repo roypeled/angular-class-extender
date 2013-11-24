5b0101b4de5836754f82c0dae9ae432bc4bfe24a
roypeled <royp@folloze.com>
2013-11-24 18:10:32 +0200

    finish?

---
+++ angular-class-extender.js
@@ -0,0 +1,64 @@
+/**!
+ * AngularJS class extender
+ * @author  Roy  <peled.roy@gmail.com>
+ * @version 1.0.0
+ */
+(function() {
+    var angularExtender = angular.module('ngExtender', []);
+
+    angularExtender.factory('$extend', ['$injector', function($injector) {
+        return function(currentScope){
+
+            var abstracts = {};
+            var bindedHandlers = [];
+            currentScope.$super = {};
+
+            currentScope.$abstract = function(name){
+                abstracts[name] = true;
+            }
+
+            currentScope.$binded = function(handler){
+                bindedHandlers.push(handler);
+            }
+
+            return {
+                with: function(){
+                    var klasses = arguments;
+                    for(var i=0; i<klasses.length; i++){
+                        $injector.instantiate(klasses[i], {$scope: currentScope});
+                        for(var func in currentScope){
+                            if(!/^(\$|this)/.test(func)){
+                                currentScope.$super[func] = currentScope[func];
+                            }
+                        }
+                    }
+
+                    setTimeout(function(){
+                        for(var func in abstracts){
+                            if(!currentScope[func]){
+
+                                // Uh-oh! It looks like someone declared an abstract but did not implement the method!
+
+                                var allNodes = document.querySelectorAll("*");
+                                var ctrl;
+                                for(var i=0; i<allNodes.length; i++){
+                                    var el = angular.element(allNodes[i]);
+                                    ctrl = el.attr("ng-controller") || el.attr("ng:controller") || el.attr("data-ng-controller");
+                                    var scope = el.scope();
+                                    if(scope == currentScope)
+                                        break;
+                                }
+                                throw new Error("Abstract method " + func + " must be overriden in "+ ctrl + "!");
+                            }
+                        }
+
+                        while(bindedHandlers.length)
+                            bindedHandlers.pop()();
+
+                        currentScope.$digest();
+                    }, 5);
+                }
+            }
+        }
+    }]);
+})();
